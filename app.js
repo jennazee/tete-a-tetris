ea3967a6383bcc0ad2673639fb6521dab5760683
Jenna Zeigen <jenna.zeigen@gmail.com>
2015-11-09 19:50:21 -0500

    Add files

---
+++ app.js
@@ -0,0 +1,23 @@
+//TETRIS
+
+function App() {
+	var gameLoop = start();
+	var game;
+}
+
+function start() {
+	game = new Game();
+	var gameLoop;
+	var speediness = 1000;
+
+	var play = function() {
+  	game.draw();
+  	setTimeout(play, speediness);
+	};
+
+	play();
+
+	return gameLoop;
+};
+
+new App();
---
+++ game.js
@@ -0,0 +1,14 @@
+//TETRIS
+
+function Game() {
+
+}
+
+Game.prototype.init = function() {
+
+}
+
+//DRAWING updates
+Game.prototype.draw = function() {
+	console.log('drawing!');
+}
